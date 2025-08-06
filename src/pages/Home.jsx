1dec07f41cf713f16b86932bd8abb317885b6858
DsFBoI <danelsf02@gmail.com>
2025-08-07 00:39:20 +0200

    carroussel optimitation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -80,6 +80,9 @@ function Home() {
               {[img1, img2, img3, img4, img5, img6,img7,img8,img9].map((img, i) => (
                 <img src={img} alt={`slide-${i}`} className="carousel-img" key={i} />
               ))}
+              {[img1, img2, img3, img4, img5, img6,img7,img8,img9].map((img, i) => (
+                <img src={img} alt={`slide-${i}-duplicate`} className="carousel-img" key={`duplicate-${i}`} />
+              ))}
             </div>
           </div>
         </section>
